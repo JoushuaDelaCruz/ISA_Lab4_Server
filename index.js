2e1adf70309a77ff134428ba37e25e9c83eb945d
oscar <97479266+o-sca@users.noreply.github.com>
2023-10-31 11:44:20 -0700

    fix querystring

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const url = require("url");
+const querystring = require("querystring");
 const { execute } = require("./database");
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
  */
 const dbQuery = async (query, statusCode, res) => {
   try {
+    console.log(query);
     const result = await execute(query);
     return res
       .writeHead(statusCode, { "Content-Type": "application/json" })
@@ -29,7 +31,7 @@ const dbQuery = async (query, statusCode, res) => {
  */
 const extractSqlQueryPath = (paths) => {
   const query = paths[paths.length - 1];
-  return query.replace(/%20/g, " ").replace(/%22/g, " ");
+  return querystring.unescape(query);
 };
 
 /**
